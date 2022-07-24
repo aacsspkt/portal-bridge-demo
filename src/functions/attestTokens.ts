b78472192b930aea6fa9e63b4a0e35fd8944c209
Ashish Sapkota <aacsspkt3@gmail.com>
2022-07-24 17:29:45 +0545

    Add function to attestTokens

---
+++ src/functions/attestTokens.ts
@@ -0,0 +1,37 @@
+import {
+	attestFromEth,
+	ChainId,
+	getEmitterAddressEth,
+	parseSequenceFromLogEth,
+	toChainName,
+} from "@certusone/wormhole-sdk";
+import { ethers } from "ethers";
+import { BRIDGE_ADDRESSES, TOKEN_BRIDGE_ADDRESS, WORMHOLE_REST_ADDRESS } from "../constants";
+import { createVaaURL } from "./createVaaUrl";
+
+/**
+ *
+ * @param sourceChain Source Chain Id
+ * @param signer Signer
+ * @param tokenAddress Token address
+ * @returns Vaa URL of contract receipt of the attestation
+ */
+export async function attestToken(sourceChain: ChainId, signer: ethers.Signer, tokenAddress: string) {
+	let tokenAttestation: ethers.ContractReceipt;
+	switch (toChainName(sourceChain)) {
+		case "ethereum":
+			const tokenBridgeAddress = TOKEN_BRIDGE_ADDRESS["ethereum"].address;
+			tokenAttestation = await attestFromEth(tokenBridgeAddress, signer, tokenAddress);
+			const emitterAddr = getEmitterAddressEth(tokenBridgeAddress);
+			const seq = parseSequenceFromLogEth(tokenAttestation, BRIDGE_ADDRESSES["ethereum"].address);
+			return createVaaURL(
+				WORMHOLE_REST_ADDRESS,
+				TOKEN_BRIDGE_ADDRESS["ethereum"].wormholeChainId.toString(),
+				emitterAddr,
+				seq,
+			);
+
+		default:
+			break;
+	}
+}
