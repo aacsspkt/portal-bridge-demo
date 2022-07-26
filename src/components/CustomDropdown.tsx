2ebc04ef71c32cf1fbe18e05d65681123a6e57cd
shraddhabhuju <Sirtombombadil@protonmail.com>
2022-07-26 13:11:10 +0545

    Dropdown Chnages

--- src/components/CustomDropdown.tsx
+++ src/components/CustomDropdown.tsx
@@ -20,10 +20,12 @@ export const CustomDropDown: FC<{
         <div ref={ref} className={`w-full relative ${className}`} tabIndex={0} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)}>
             <div className='border p-1 h-9'>
                 <span className='text-md capitalize'>{value}</span>
+               
+ 
             </div>
             {
                 isFocused &&
-                <div className='mt-2 bg-gray-100 absolute z-10 w-full border'>
+                <div className='mt-2 bg-gray-100 max-h-40 overflow-y-auto z-10 w-full border'>
                     <ul className=''>
                         {
                             dropdownList.length > 0 &&
@@ -40,6 +42,6 @@ export const CustomDropDown: FC<{
                     </ul>
                 </div>
             }
-        </div >
+                   </div >
     )
 }
