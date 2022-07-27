7799853558536303c1a16d0a026274e642d781fd
Ashish Sapkota <aacsspkt3@gmail.com>
2022-07-27 18:50:26 +0545

    Update attest functions

--- src/functions/attestTokens.ts
+++ src/functions/attestTokens.ts
@@ -1,22 +1,18 @@
-import base58 from 'bs58';
 import { ethers } from 'ethers';
 
 import {
   attestFromEth,
   ChainName,
   getEmitterAddressEth,
+  getSignedVAAWithRetry,
   parseSequenceFromLogEth,
 } from '@certusone/wormhole-sdk';
-import {
-  Keypair,
-  PublicKey,
-} from '@solana/web3.js';
 
 import {
   BRIDGE_ADDRESS_TESTNET,
   TOKEN_BRIDGE_ADDRESS_TESTNET,
+  WORMHOLE_REST_ADDRESS_TESTNET,
 } from '../constants_testnet';
-import { createWrappedTokens } from './createWrapped';
 
 export * from "./createWrapped";
 
@@ -27,33 +23,23 @@ export * from "./createWrapped";
  * @param tokenAddress Token address
  * @returns Vaa URL of contract receipt of the attestation
  */
-export async function attestToken(
-	sourceChain: ChainName,
-	signer: ethers.Signer,
-	tokenAddress: string,
-	payerAddress: PublicKey,
-) {
-	let tokenAttestation: ethers.ContractReceipt;
+export async function attestToken(sourceChain: ChainName, signer: ethers.Signer, tokenAddress: string) {
 	switch (sourceChain) {
 		case "ethereum":
-			let gasLim: ethers.BigNumberish = 1000000;
+			// let gasLim: ethers.BigNumberish = 1000000;
 			const tokenBridgeAddress = TOKEN_BRIDGE_ADDRESS_TESTNET["ethereum"].address;
-			tokenAttestation = await attestFromEth(tokenBridgeAddress, signer, tokenAddress, { gasLimit: gasLim });
-			const emitterAddr = getEmitterAddressEth(tokenBridgeAddress);
-			const seq = parseSequenceFromLogEth(tokenAttestation, BRIDGE_ADDRESS_TESTNET["ethereum"].address);
-			console.log("======  Getting signed VAA ======");
-			const vaaURL = `https://wormhole-v2-testnet-api.certus.one/v1/signed_vaa/2/000000000000000000000000f890982f9310df57d00f659cf4fd87e65aded8d7/1544`;
-			console.log("Searching for: ", vaaURL);
-			let vaaBytes = await (await fetch(vaaURL)).json();
-			while (!vaaBytes.vaaBytes) {
-				console.log("VAA not found, retrying in 5s!");
-				await new Promise((r) => setTimeout(r, 5000)); //Timeout to let Guardiand pick up log and have VAA ready
-				vaaBytes = await (await fetch(vaaURL)).json();
-			}
-			const keypair = Keypair.fromSecretKey(base58.decode(process.env.REACT_APP_WALLET_SECRET_KEY as string));
-			return createWrappedTokens(sourceChain, payerAddress, keypair, vaaBytes.vaaBytes);
+			const tokenAttestation = await attestFromEth(tokenBridgeAddress, signer, tokenAddress);
+			const emitterAddress = getEmitterAddressEth(tokenBridgeAddress);
+			const sequence = parseSequenceFromLogEth(tokenAttestation, BRIDGE_ADDRESS_TESTNET["ethereum"].address);
+			const { vaaBytes } = await getSignedVAAWithRetry(
+				[WORMHOLE_REST_ADDRESS_TESTNET],
+				sourceChain,
+				emitterAddress,
+				sequence,
+			);
+			return vaaBytes;
 
 		default:
-			break;
+			return null;
 	}
 }
