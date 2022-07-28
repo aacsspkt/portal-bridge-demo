f1aa51a7326f4757da6039a14067f561877a4bbb
Ashish Sapkota <aacsspkt3@gmail.com>
2022-07-28 10:31:15 +0545

    Update createWrapped functions

--- src/functions/createWrapped.ts
+++ src/functions/createWrapped.ts
@@ -1,14 +1,23 @@
-import { ethers } from "ethers";
-
-import { ChainName, createPostVaaInstructionSolana, createWrappedOnSolana } from "@certusone/wormhole-sdk";
-import { Keypair, SendTransactionError, Transaction } from "@solana/web3.js";
+import { ethers } from 'ethers';
 
 import {
-	BRIDGE_ADDRESS_TESTNET,
-	CONNECTION_TESTNET as connection,
-	TOKEN_BRIDGE_ADDRESS_TESTNET,
-} from "../constants_testnet";
-import { sendAndConfirmTransactions } from "./sendTransactionSolana";
+  ChainName,
+  createWrappedOnSolana,
+  postVaaSolanaWithRetry,
+} from '@certusone/wormhole-sdk';
+import {
+  Keypair,
+  SendTransactionError,
+  Transaction,
+} from '@solana/web3.js';
+
+import {
+  BRIDGE_ADDRESS_TESTNET,
+  CONNECTION_TESTNET as connection,
+  RECIPIENT_WALLET_ADDRESS_TESTNET,
+  TOKEN_BRIDGE_ADDRESS_TESTNET,
+} from '../constants_testnet';
+import { sendAndConfirmTransactions } from './sendTransactionSolana';
 
 /**
  * @param sourceChain Source Chain Name
@@ -29,11 +38,26 @@ export async function createWrappedTokens(
 				if (!(signer instanceof Keypair)) throw new Error(`Signer should be instanceof Keypair. value: ${signer}`);
 				const bridgeAddress = BRIDGE_ADDRESS_TESTNET["solana"].address;
 				const tokenBridgeAddress = TOKEN_BRIDGE_ADDRESS_TESTNET["solana"].address;
+				const signTransaction = async (transaction: Transaction) => {
+					const existingPair = transaction.signatures.filter((pair) => pair.signature !== null);
+					transaction.sign(signer);
+					existingPair.forEach((pair) => {
+						if (pair.signature) transaction.addSignature(pair.publicKey, pair.signature);
+					});
+					return transaction;
+				};
+
 				//post vaa
-				console.log("creating txn for posting vaa");
-				const postVaaTxn = new Transaction().add(
-					await createPostVaaInstructionSolana(bridgeAddress, payerAddress, Buffer.from(signedVAA), signer),
+				console.log("posting vaa to solana");
+				await postVaaSolanaWithRetry(
+					connection,
+					signTransaction,
+					bridgeAddress,
+					RECIPIENT_WALLET_ADDRESS_TESTNET.toString(),
+					Buffer.from(signedVAA),
+					10,
 				);
+
 				console.log("creating txn to create wrapped token");
 				// create wrapped tokens
 				const createWrappedTxn = await createWrappedOnSolana(
@@ -43,9 +67,9 @@ export async function createWrappedTokens(
 					payerAddress,
 					signedVAA,
 				);
-				console.log("init txn send.");
-				const txnIds = await sendAndConfirmTransactions(connection, [postVaaTxn, createWrappedTxn], signer);
-				return txnIds;
+				await sendAndConfirmTransactions(connection, [createWrappedTxn], RECIPIENT_WALLET_ADDRESS_TESTNET, [signer]);
+
+				return;
 			} catch (error) {
 				if (error instanceof SendTransactionError) {
 					console.log(error.logs);
