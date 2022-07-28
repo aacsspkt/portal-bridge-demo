523f2ef0f95a785004f2837bcb024cc9846b15a1
roshans <shrestharoshan768@gmail.com>
2022-07-28 23:05:21 +0545

    add isWrapped for solana

--- src/functions/isWrapped.ts
+++ src/functions/isWrapped.ts
@@ -3,17 +3,39 @@ import { ethers } from 'ethers';
 import {
   ChainId,
   getIsWrappedAssetEth,
+  getIsWrappedAssetSol,
   toChainName,
 } from '@certusone/wormhole-sdk';
 
-import { TOKEN_BRIDGE_ADDRESS } from '../constants';
+import { BRIDGE_ADDRESS_TESTNET, TOKEN_BRIDGE_ADDRESS_TESTNET } from '../constants_testnet';
 
-export async function isWrapped(sourceChainId: ChainId, signer: ethers.Signer, tokenAddress: string) {
+import {Connection} from "@solana/web3.js";
+
+export async function isWrapped(
+	sourceChainId: ChainId, 
+	signer: ethers.Signer, 
+	tokenAddress: string, 
+	connection?:Connection) {
 	switch (toChainName(sourceChainId)) {
 		case "ethereum": {
-			const is_wrapped = getIsWrappedAssetEth(TOKEN_BRIDGE_ADDRESS["ethereum"].address, signer, tokenAddress);
+			const is_wrapped = getIsWrappedAssetEth(
+				TOKEN_BRIDGE_ADDRESS_TESTNET["ethereum_goerli"].address, 
+				signer, 
+				tokenAddress
+				);
 			console.log("Token Wrapped? ==> ", is_wrapped);
 			return is_wrapped;
 		}
+		case "solana": {
+			const is_wrapped = getIsWrappedAssetSol(
+				connection as Connection, 
+				BRIDGE_ADDRESS_TESTNET["solana"].address, 
+				tokenAddress
+				);
+			console.log("Token Wrapped? ==> ", is_wrapped);
+			return is_wrapped;
+		}
+		default:
+			return null;
 	}
 }
