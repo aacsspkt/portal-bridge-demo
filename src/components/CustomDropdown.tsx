b79e227b7411b722ee49aa4682244fb7f4c8b39c
Ashish Sapkota <aacsspkt3@gmail.com>
2022-08-04 17:51:53 +0545

    Upgrade Dropdown

--- src/components/CustomDropdown.tsx
+++ src/components/CustomDropdown.tsx
@@ -1,40 +1,41 @@
-import React, {
-  FC,
+import {
   useRef,
   useState,
 } from 'react';
 
-export const CustomDropDown: FC<{
-    className?: string,
-    dropdownList: string[]
-    value: string,
-    onChange: Function
-}> = ({ className, dropdownList, value, onChange }) => {
+interface DropDownProps<T extends string | Object, K extends string | Object> {
+    className?: string | undefined;
+    options: T[];
+    value: T;
+    getLabel: (value: T) => K;
+    onChange: (value: T) => void;
+}
+
+export default function CustomDropDown<T extends string | Object, K extends string | Object>(props: DropDownProps<T, K>) {
+    const { className, options, value, getLabel, onChange } = props;
     const [isFocused, setIsFocused] = useState(false);
     const ref = useRef<HTMLDivElement>(null);
-    const onSelected = (selected: string) => {
+    const onSelected = (selected: T) => {
         onChange(selected);
         ref.current?.blur();
     };
     return (
         <div ref={ref} className={`w-full relative ${className}`} tabIndex={0} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)}>
             <div className='border p-1 h-9'>
-                <span className='text-md capitalize'>{value}</span>
-               
- 
+                <span className='text-md capitalize'>{value.toString()}</span>
             </div>
             {
                 isFocused &&
                 <div className='mt-2 bg-gray-100 max-h-40 overflow-y-auto z-10 w-full border'>
                     <ul className=''>
                         {
-                            dropdownList.length > 0 &&
-                            dropdownList.map((option) => {
+                            options.length > 0 &&
+                            options.map((option, i) => {
                                 return (
                                     <li className='w-full capitalize p-1 cursor-pointer hover:bg-gray-200'
-                                        key={option}
+                                        key={i}
                                         onClick={(e) => onSelected(option)}>
-                                        {option}
+                                        {getLabel(option).toString()}
                                     </li>
                                 )
                             })
@@ -42,6 +43,6 @@ export const CustomDropDown: FC<{
                     </ul>
                 </div>
             }
-                   </div >
+        </div >
     )
 }
