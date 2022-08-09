61f4eab9d7a10067f9a930b300e8a21434c30091
Ashish Sapkota <aacsspkt3@gmail.com>
2022-08-09 16:17:38 +0545

    Remove unused functions

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,6 +1,5 @@
 export * from "./attestTokens";
 export * from "./deriveCorrespondingToken";
-export * from "./transferTokens";
 export * from "./tokenValidation";
 export * from "./createWrapped";
 export * from "./sendPayloadToSolana";
